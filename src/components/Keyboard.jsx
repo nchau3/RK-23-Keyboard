d3d0f4b9f316d8199839b5877b844db7e2c1eb1a
Nicholas <nc_chau@hotmail.com>
2023-02-20 12:39:57 -0500

    Controls component, styling for main gain slider

--- src/components/Keyboard.jsx
+++ src/components/Keyboard.jsx
@@ -1,9 +1,13 @@
+//components
+import Controls from "./Controls";
+
 //styles
 import "../styles/keyboard.scss";
 
 export default function Keyboard() {
   return (
     <div className="keyboard">
+      <Controls />
       <div className="keys-container">
 
       </div>
