a42dad7a0e93c5242fc1d2608f28ff8262319f6e
Nicholas <nc_chau@hotmail.com>
2023-02-20 12:58:44 -0500

    playTone function, create single oscillator

--- src/components/Keyboard.jsx
+++ src/components/Keyboard.jsx
@@ -2,10 +2,24 @@
 import Controls from "./Controls";
 import Key from "./Key";
 
+//hooks
+import useAudioContext from "../useAudioContext";
+
 //styles
 import "../styles/component-styles/keyboard.scss";
 
 export default function Keyboard() {
+  const { audioContext, mainGainNode, oscList, volume, changeVolume, noteFreq } = useAudioContext();
+
+  const playTone = (freq) => {
+    const osc = audioContext.createOscillator();
+    osc.frequency.value = freq;
+    osc.connect(mainGainNode);
+    osc.start();
+
+    return osc;
+  }
+
   return (
     <div className="keyboard">
       <Controls />
--- src/useAudioContext.js
+++ src/useAudioContext.js
@@ -1,9 +1,13 @@
 import { useState } from "react";
+import createNoteTable from "./noteTable";
 
 //context and main nodes declared outside of component
 const audioContext = new AudioContext();
 const mainGainNode = audioContext.createGain();
 mainGainNode.connect(audioContext.destination);
+
+//note frequencies array
+const noteFreq = createNoteTable();
 
 //set empty octave objects to manage active pitches
 const oscList = [];
@@ -21,5 +25,5 @@ export default function useAudioContext() {
     setVolume(newValue);
   }
 
-  return { audioContext, mainGainNode, oscList, volume, changeVolume };
+  return { audioContext, mainGainNode, oscList, volume, changeVolume, noteFreq };
 }
