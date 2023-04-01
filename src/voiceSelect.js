8bc0c9ca7c4f7c24c1203460dd3c12679486abd1
Nicholas <nc_chau@hotmail.com>
2023-03-31 21:28:25 -0400

    cut high overtones on all voices

--- src/voiceSelect.js
+++ src/voiceSelect.js
@@ -1,14 +1,14 @@
 export const voice1 = {
   type: "sine",
-  harmonics: [1, 1, 0.6, 1.3, 0.25, 0.45, 0.35]
+  harmonics: [1, 1, 0.6, 1.3, 0.025, 0.045, 0.035]
 };
 
 export const voice2 = {
-  type: "sawtooth",
-  harmonics: [1.4, 0.1, 0.8, 1, 0.9, 0.2, 0.3, 0.1]
+  type: "sine",
+  harmonics: [1.4, 0.1, 0.8, 0.06, 0.09, 0.02, 0.003, 0.003, 0.001, 0.001, 0.002]
 };
 
 export const voice3 = {
   type: "triangle",
-  harmonics: [1.5, 1.5, 1, 1.1, 1.3, 1.2, 0.1, 0.2, 0.1, 0.1, 0.1]
+  harmonics: [1, 0.7, 1, 0.1, 0.7, 0.05, 0.001, 0.002, 0.001, 0.001, 0.001]
 };
