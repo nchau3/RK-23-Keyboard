be58cb9eb59453ee120d6b94fc17842ad0287aee
Nicholas <nc_chau@hotmail.com>
2023-04-10 17:53:53 -0400

    rename voices

--- src/voiceSelect.js
+++ src/voiceSelect.js
@@ -12,7 +12,7 @@ class Voice {
 const voiceLibrary = [
   new Voice(
     1,
-    "first",
+    "organ",
     [1, 1, 0.6, 1.3, 0.02, 0.04, 0.03],
     {
       attack: 0.001,
@@ -21,7 +21,7 @@ const voiceLibrary = [
   ),
   new Voice(
     2,
-    "second",
+    "clarinet",
     [1.4, 0.1, 0.8, 0.06, 0.09, 0.02, 0.003, 0.003, 0.001, 0.001, 0.002],
     {
       attack: 0.01,
@@ -30,7 +30,7 @@ const voiceLibrary = [
   ),
   new Voice(
     3,
-    "third",
+    "accordion",
     [1.7, 1.4, 0.07, 0.05, 0.003, 0.1, 0.001, 0.002, 0.01, 0.01, 0.001, 0.002, 0.006],
     {
       attack: 0.2,
